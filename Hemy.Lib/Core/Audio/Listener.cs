b5ec3bf6ee296a673ba8253acccc8d9ee100f79b
Grom76 <gauthier_jerome@live.Fr>
2025-09-15 17:27:05 +0200

    Add Memory Array

---
+++ Hemy.Lib/Core/Audio/Listener.cs
@@ -0,0 +1,40 @@
+
+
+namespace Hemy.Lib.Core.Audio;
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Hemy.Lib.Core.Math;
+
+[SkipLocalsInit]
+[StructLayout(LayoutKind.Sequential)]
+public struct AudioListener
+{
+    /// <summary>
+    /// Determine la position point dans l'esapce de l'écouteur
+    /// </summary>
+    public Vector3 Position;
+
+    /// <summary>
+    ///  determine la vitesse de déplacement de l'écouteur
+    /// exemple dans un fps l'écouteur c'est le joueur et il ce déplace
+    /// </summary>
+    public Vector3 Velocity;
+
+    /// <summary>
+    /// Détermine l'orientation de l'écouteur ( cone comme la caméra )
+    /// </summary>    
+    public Vector3 Target;// Orientation
+    /// <summary>
+    /// Détermine le postionnnement tête haute de l'écouteur ( cone comme la caméra )
+    /// </summary>    
+    public Vector3 Up;
+
+    /// <summary>
+    ///  Volume général du son mini 0.0f, maxi 1.0f
+    /// </summary>
+    public float Volume;
+
+    //Ambient oclusion
+}
+   
